c1e3e3b43660b1a0ab4df08c68088e7e50ac2779
sparshkr24 <sparsh24112002@gmail.com>
2023-11-01 22:18:20 +0530

    Setup frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,43 @@
+import { ChakraProvider } from '@chakra-ui/react'
+
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from './Layout';
+import AddStudents from './pages/AddStudents';
+import MyStudents from './pages/MyStudents';
+import Submission from './pages/Submission';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout>
+      <AddStudents />
+    </Layout>,
+  },
+  {
+    path: "/mystudents",
+    element: <Layout><MyStudents /></Layout>,
+  },
+  {
+    path: "/submissions",
+    element: <Layout><Submission /></Layout>,
+  },
+  {
+    path: "/*",
+    element: <div>404 not found</div>
+  }
+]);
+
+function App() {
+  return (
+    <ChakraProvider>
+      <RouterProvider router={router} />
+    </ChakraProvider>
+  );
+}
+
+export default App;
